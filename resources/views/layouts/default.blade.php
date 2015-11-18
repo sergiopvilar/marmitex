202a945f7e7c4a38dc9bb5d1f60ddf7c36f6805c
Sergio Vilar <sergiovilar.r@gmail.com>
2015-11-18 17:43:38 -0300

    Atualizado layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,13 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Marmitex</title>
-    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="\assets/css/main.css" rel="stylesheet">
   </head>
   <body>
-    
-    <h1>Hello, world!</h1>
 
-    <script src="assets/js/vendor.js"></script>
-    <script src="assets/js/scripts.js"></script>
+      <div class="container">
+        @yield('content')
+      </div>
+
+  <script src="\assets/js/vendor.js"></script>
+    <script src="\assets/js/scripts.js"></script>
   </body>
 </html>
